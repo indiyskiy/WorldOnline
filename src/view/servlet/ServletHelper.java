bf5b693e68c6c9a8de8dd8358f88da4afd6b6580
Evdokimov Ilia <indiyskiy@gmail.com>
2014-03-03 14:48:53 +0400

    card menu link revenge!

--- src/view/servlet/ServletHelper.java
+++ src/view/servlet/ServletHelper.java
@@ -1,11 +1,15 @@
 package view.servlet;
 
+import controller.phone.parser.MobileErrorParser;
+import model.logger.LoggerFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -16,25 +20,55 @@ import java.io.UnsupportedEncodingException;
  * To change this template use File | Settings | File Templates.
  */
 public class ServletHelper {
+
     public static void sendForward(String url, HttpServlet servlet, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = servlet.getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
     }
 
     public static void setUTF8(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
-        response.setContentType ("text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
     }
 
-    public static String getAndSetAttribute(HttpServletRequest request, String name){
-        if(name==null){
-          return null;
+    public static String getAndSetAttribute(HttpServletRequest request, String name) {
+        if (name == null) {
+            return null;
         }
-        String param=request.getParameter(name);
-        if(param!=null){
-            name=name.substring(0,1).toLowerCase()+name.substring(1);
-            request.setAttribute(name,param);
+        String param = request.getParameter(name);
+        if (param != null) {
+            name = name.substring(0, 1).toLowerCase() + name.substring(1);
+            request.setAttribute(name, param);
         }
         return param;
+    }
+
+    public static void sendResponse(HttpServletResponse response, String responseString) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println(responseString);
+        out.flush();
+        out.close();
+    }
+
+    public static void sendError(Exception error, HttpServletRequest request, HttpServletResponse response, HttpServlet servlet, LoggerFactory loggerFactory) {
+        try {
+            request.setAttribute("errorMesage", error.getMessage());
+            loggerFactory.error(error);
+            ServletHelper.sendForward("/error.jsp", servlet, request, response);
+        } catch (ServletException e) {
+            loggerFactory.error(e);
+        } catch (IOException e) {
+            loggerFactory.error(e);
+        }
+    }
+
+    public static void sendMobileError(LoggerFactory loggerFactory, Exception error, HttpServletResponse response) {
+        loggerFactory.error(error);
+        String errorJSON= MobileErrorParser.parse(error);
+        try {
+            sendResponse(response, errorJSON);
+        } catch (IOException e) {
+            loggerFactory.error(e);
+        }
     }
 }
