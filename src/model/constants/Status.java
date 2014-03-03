eac30cabdb40d717ac4d98c0fdd1dd447e61589a
Evdokimov Ilia <indiyskiy@gmail.com>
2014-03-03 14:45:38 +0400

    card menu link revenge!

---
+++ src/model/constants/Status.java
@@ -0,0 +1,22 @@
+package model.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Servcer
+ * Date: 28.02.14
+ * Time: 16:39
+ * To change this template use File | Settings | File Templates.
+ */
+public enum  Status {
+    Error(0),
+    OK(1);
+    private final int value;
+
+    Status(int value) {
+        this.value=value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
